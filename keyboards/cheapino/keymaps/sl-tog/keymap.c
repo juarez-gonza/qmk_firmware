b3afeafcd599acc1608c6ba769d018c3fc34f6d6
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2025-11-25 23:10:35 +0100

    place keys at dvorak pos of s and l in third thumb key left and right resp.

--- keyboards/cheapino/keymaps/sl-tog/keymap.c
+++ keyboards/cheapino/keymaps/sl-tog/keymap.c
@@ -29,40 +29,40 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x5_3(
-    KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,  /* ===== */ KC_F,  KC_G,     KC_C,  KC_R, KC_L,
-    KC_A,    KC_O,    KC_E,    KC_U,   KC_I,  /* ===== */ KC_D,  KC_H,     KC_T,  KC_N, KC_S,
-    KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,  /* ===== */ KC_B,  KC_M,     KC_W,  KC_V, KC_Z,
-                      KC_BSPC, OS_SYM, KC_NO, /* ===== */ KC_NO, OS_NAV, KC_SPC
+    KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y, /* ===== */ KC_F, KC_G,   KC_C,  KC_R, KC_L,
+    KC_A,    KC_O,    KC_E,    KC_U,   KC_I, /* ===== */ KC_D, KC_H,   KC_T,  KC_N, KC_S,
+    KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X, /* ===== */ KC_B, KC_M,   KC_W,  KC_V, KC_Z,
+                      KC_BSPC, OS_SYM, KC_L, /* ===== */ KC_S, OS_NAV, KC_SPC
   ),
 
   // NAV LAYER
 
   [NAV] = LAYOUT_split_3x5_3(
-    KC_NO, KC_PGUP,  KC_UP,   KC_PGDN, KC_HOME, /* ===== */ KC_NO, KC_CAPS,     KC_TAB,  KC_DEL, KC_NO,
-    KC_NO, KC_LEFT,  KC_DOWN, KC_RGHT, KC_END,  /* ===== */ KC_NO, OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
-    KC_NO, OS_CANCEL, KC_ESC,  KC_ENT, KC_NO,   /* ===== */ KC_NO, OS_FUN,      KC_NO,   KC_NO,  KC_NO,
-                     KC_TRNS, OS_SYM,  KC_NO,   /* ===== */ KC_NO, TG(NAV + 1), KC_TRNS
+    KC_NO, KC_PGUP,  KC_UP,   KC_PGDN, KC_HOME, /* ===== */ KC_NO,  KC_CAPS,     KC_TAB,  KC_DEL, KC_NO,
+    KC_NO, KC_LEFT,  KC_DOWN, KC_RGHT, KC_END,  /* ===== */ KC_NO,  OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
+    KC_NO, OS_CANCEL, KC_ESC,  KC_ENT, KC_NO,   /* ===== */ KC_NO,  OS_FUN,      KC_NO,   KC_NO,  KC_NO,
+                     KC_TRNS, OS_SYM,  KC_NO,   /* ===== */ OS_GUI, TG(NAV + 1), KC_TRNS
   ),
   [NAV + 1] = LAYOUT_split_3x5_3(
-    KC_NO, KC_PGUP,     KC_UP,   KC_PGDN, KC_HOME, /* ===== */ KC_NO, KC_CAPS,     KC_TAB,  KC_DEL, KC_NO,
-    KC_NO, KC_LEFT,     KC_DOWN, KC_RGHT, KC_END,  /* ===== */ KC_NO, OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
-    KC_NO, TG(NAV + 1), KC_ESC,  KC_ENT,  KC_NO,   /* ===== */ KC_NO, OS_FUN,      KC_NO,   KC_NO,  KC_NO,
-                        KC_TRNS, OS_SYM,  KC_NO,   /* ===== */ KC_NO, TG(NAV + 1), KC_TRNS
+    KC_NO, KC_PGUP,     KC_UP,   KC_PGDN, KC_HOME, /* ===== */ KC_NO,  KC_CAPS,     KC_TAB,  KC_DEL, KC_NO,
+    KC_NO, KC_LEFT,     KC_DOWN, KC_RGHT, KC_END,  /* ===== */ KC_NO,  OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
+    KC_NO, TG(NAV + 1), KC_ESC,  KC_ENT,  KC_NO,   /* ===== */ KC_NO,  OS_FUN,      KC_NO,   KC_NO,  KC_NO,
+                        KC_TRNS, OS_SYM,  KC_NO,   /* ===== */ OS_GUI, TG(NAV + 1), KC_TRNS
   ),
 
   // FUN LAYER
 
   [FUN] = LAYOUT_split_3x5_3(
-    KC_NO, KC_F1,     KC_F2,   KC_F3,   KC_F4,   /* ===== */ KC_F5, KC_F6,       KC_F7,   KC_F8,  KC_F9,
-    KC_NO, KC_MUTE,   KC_VOLD, KC_VOLU, KC_PSCR, /* ===== */ KC_NO, OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
-    KC_NO, OS_CANCEL, KC_BRID, KC_BRIU, KC_NO,   /* ===== */ KC_NO, TG(FUN + 1), KC_NO,   KC_NO,  KC_NO,
-                      KC_TRNS, OS_SYM,  KC_NO,   /* ===== */ KC_NO, KC_NO,       KC_TRNS
+    KC_NO, KC_F1,     KC_F2,   KC_F3,   KC_F4,   /* ===== */ KC_F5,  KC_F6,       KC_F7,   KC_F8,  KC_F9,
+    KC_NO, KC_MUTE,   KC_VOLD, KC_VOLU, KC_PSCR, /* ===== */ KC_NO,  OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
+    KC_NO, OS_CANCEL, KC_BRID, KC_BRIU, KC_NO,   /* ===== */ KC_NO,  TG(FUN + 1), KC_NO,   KC_NO,  KC_NO,
+                      KC_TRNS, OS_SYM,  KC_F9,   /* ===== */ OS_GUI, KC_NO,       KC_TRNS
   ),
   [FUN + 1] = LAYOUT_split_3x5_3(
-    KC_NO, KC_F1,       KC_F2,   KC_F3,    KC_F4,   /* ===== */ KC_F5, KC_F6,       KC_F7,   KC_F8,  KC_F9,
-    KC_NO, KC_MUTE,     KC_VOLD, KC_VOLU,  KC_PSCR, /* ===== */ KC_NO, OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
-    KC_NO, TG(FUN + 1), KC_BRID, KC_BRIU,  KC_NO,   /* ===== */ KC_NO, TG(FUN + 1), KC_NO,   KC_NO,  KC_NO,
-                        KC_TRNS, OS_SYM, KC_NO,   /* ===== */ KC_NO, TG(FUN + 1), KC_TRNS
+    KC_NO, KC_F1,       KC_F2,   KC_F3,    KC_F4,   /* ===== */ KC_F5,  KC_F6,       KC_F7,   KC_F8,  KC_F9,
+    KC_NO, KC_MUTE,     KC_VOLD, KC_VOLU,  KC_PSCR, /* ===== */ KC_NO,  OS_SFT,      OS_CTRL, OS_ALT, OS_GUI,
+    KC_NO, TG(FUN + 1), KC_BRID, KC_BRIU,  KC_NO,   /* ===== */ KC_NO,  TG(FUN + 1), KC_NO,   KC_NO,  KC_NO,
+                        KC_TRNS, OS_SYM,   KC_F9,   /* ===== */ OS_GUI, TG(FUN + 1), KC_TRNS
   ),
 
   // SYM LAYER
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,                   KC_TILD,   KC_QUES, KC_UNDS,     KC_NO, /* ===== */   KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_NO,
     OS_GUI,                  OS_ALT,    OS_CTRL, OS_NUM,      KC_NO, /* ===== */   KC_RCBR, KC_DLR,  KC_PERC, KC_CIRC, KC_RPRN,
     CPP_NAMESPACE_SEPARATOR, OS_CANCEL, KC_PIPE, KC_PLUS,     KC_NO, /* ===== */   KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_NO,
-                                        KC_TRNS, TG(SYM + 1), KC_NO, /* ===== */   KC_NO,   KC_ENT,  KC_TRNS
+                                        KC_TRNS, TG(SYM + 1), KC_NO, /* ===== */   KC_RPRN, KC_ENT,  KC_TRNS
   ),
   [SYM + 1] = LAYOUT_split_3x5_3(
     KC_NO,                   KC_TILD,     KC_QUES, KC_UNDS,     KC_NO, /* ===== */   KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_NO,
     OS_GUI,                  OS_ALT,      OS_CTRL, OS_NUM,      KC_NO, /* ===== */   KC_RCBR, KC_DLR,  KC_PERC, KC_CIRC, KC_RPRN,
     CPP_NAMESPACE_SEPARATOR, TG(SYM + 1), KC_PIPE, KC_PLUS,     KC_NO, /* ===== */   KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_NO,
-                                          KC_TRNS, TG(SYM + 1), KC_NO, /* ===== */   KC_NO,   KC_ENT,  KC_TRNS
+                                          KC_TRNS, TG(SYM + 1), KC_NO, /* ===== */   KC_RPRN, KC_ENT,  KC_TRNS
   ),
 
   // NUM LAYER
@@ -86,13 +86,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,            KC_GRV,    KC_SLSH, KC_MINS,     KC_NO, /* ===== */ KC_LBRC, KC_7,    KC_8,   KC_9, KC_NO,
     OS_GUI,           OS_ALT,    OS_CTRL, TG(NUM + 1), KC_NO, /* ===== */ KC_RBRC, KC_4,    KC_5,   KC_6, KC_0,
     C_ARROW_OPERATOR, OS_CANCEL, KC_BSLS, KC_EQL,      KC_NO, /* ===== */ KC_NO,   KC_1,    KC_2,   KC_3, KC_NO,
-                                 KC_TRNS, TG(NUM + 1), KC_NO, /* ===== */ KC_NO,   KC_ENT,  KC_TRNS
+                                 KC_TRNS, TG(NUM + 1), KC_NO, /* ===== */ KC_0,    KC_ENT,  KC_TRNS
   ),
   [NUM + 1] = LAYOUT_split_3x5_3(
     KC_NO,            KC_GRV,      KC_SLSH, KC_MINS,     KC_NO, /* ===== */ KC_LBRC, KC_7,    KC_8, KC_9, KC_NO,
     OS_GUI,           OS_ALT,      OS_CTRL, OS_SFT,      KC_NO, /* ===== */ KC_RBRC, KC_4,    KC_5, KC_6, KC_0,
     C_ARROW_OPERATOR, TG(NUM + 1), KC_BSLS, KC_EQL,      KC_NO, /* ===== */ KC_NO,   KC_1,    KC_2, KC_3, KC_NO,
-                                   KC_TRNS, TG(NUM + 1), KC_NO, /* ===== */ KC_NO,   KC_ENT,  KC_TRNS
+                                   KC_TRNS, TG(NUM + 1), KC_NO, /* ===== */ KC_0,    KC_ENT,  KC_TRNS
   )
 };
 
